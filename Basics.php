b4189e74ca4046d12183d5de530fe4c0c1977fb0
Jeevitha <your-email@gmail.com>
2026-01-12 16:22:22 +0530

    Added PHP basics: variables, if-else and for loop

---
+++ Basics.php
@@ -0,0 +1,22 @@
+<?php
+// Variables
+$name="Jeevitha";
+$dept="IT";
+echo "Name : $name\n";
+echo "Department : $dept\n";
+
+// if-else
+$mark=65;
+if($mark>=50){
+    echo "You are pass";
+}else{
+    echo "You are fail retake test";
+}
+
+echo "\nNow studying loops...\n";
+echo "Use for loop to print 1 to 10...\n";
+//loops
+//for loops
+for($i=1;$i<=10;$i++){
+    echo $i."\n";
+}
